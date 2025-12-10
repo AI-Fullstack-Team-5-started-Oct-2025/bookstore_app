d5522520a848e88172c1596ec5fae3183fb1b21d
KIM TAEK KWON <cheng80@nate.com>
2025-12-10 13:25:04 +0900

    ZeroTest 클래스 생성

---
+++ lib/view/test_zero.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TestZero extends StatefulWidget {
+  const TestZero({super.key});
+
+  @override
+  State<TestZero> createState() => _TestZeroState();
+}
+
+class _TestZeroState extends State<TestZero> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
