0f43db798f46da03df43a23cddc435b52709950d
CubeMaterial <maestro.master03@gmail.com>
2025-12-11 16:22:22 +0900

    Cheng (#14)
    * customer.dart수정, employee.dart생성
    
    * customer, employee add Function toMap
    
    * customer, employee var rename
    
    ---------
    
    Co-authored-by: KIM TAEK KWON <cheng80@nate.com>

--- lib/model/employee.dart
+++ lib/model/employee.dart
@@ -3,6 +3,7 @@
   Create: 12/11/2025 15:38, Creator: taekkwon, kim
   Update log: 
     12/11/2025 15:38, 'create Employee Model', Creator: taekkwon, kim
+    12/11/2025 15:38, 'add Function toMap', Creator: taekkwon, kim
   Version: 1.0
   Dependency: SQFlite, Path, collection
   Desc: Employee Model
@@ -11,32 +12,47 @@
 class Employee {
   // Properties
   int? id;
-  final String email;
-  final String phoneNumber;
-  final String name;
-  final String password;
+  final String eEmail;
+  final String ePhoneNumber;
+  final String eName;
+  final String ePassword;
 
   // Constructor
   Employee({
     this.id,
-    required this.email,
-    required this.phoneNumber,
-    required this.name,
-    required this.password,
+    required this.eEmail,
+    required this.ePhoneNumber,
+    required this.eName,
+    required this.ePassword,
   });
 
   Employee.fromMap(Map<String, Object?> map)
     : id = map['id'] as int?,
-      email = map['email'] as String,
-      phoneNumber = map['phoneNumber'] as String,
-      name = map['name'] as String,
-      password = map['password'] as String;
+      eEmail = map['eEmail'] as String,
+      ePhoneNumber = map['ePhoneNumber'] as String,
+      eName = map['eName'] as String,
+      ePassword = map['ePassword'] as String;
+
+  Map<String, Object?> toMap({bool includeId = false}) {
+    final map = <String, Object?>{
+      'eEmail': eEmail,
+      'ePhoneNumber': ePhoneNumber,
+      'eName': eName,
+      'ePassword': ePassword,
+    };
+
+    if (includeId) {
+      map['id'] = id;
+    }
+
+    return map;
+  }
 
   static const List<String> keys = [
     'id',
-    'email',
-    'phoneNumber',
-    'name',
-    'password',
+    'eEmail',
+    'ePhoneNumber',
+    'eName',
+    'ePassword',
   ];
 }
