c7d6edae0e1a27549485ca862a407cf829ea8c10
KIM TAEK KWON <cheng80@nate.com>
2025-12-12 22:23:21 +0900

    메시지 제거

--- lib/view/cheng/login_screen.dart
+++ lib/view/cheng/login_screen.dart
@@ -56,7 +56,6 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   void initState() {
     super.initState();
-    print("히히히히");
     // DB 초기화는 main.dart에서 이미 수행되므로 여기서는 호출하지 않습니다.
   }
 
